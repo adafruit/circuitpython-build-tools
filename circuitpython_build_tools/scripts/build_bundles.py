03373f49a9e3f7e84d9191aa0ab700ce511faa53
sommersoft <sommersoft@gmail.com>
2019-05-07 20:39:07 -0500

    set @click 'package_folder_prefix' default to 'adafruit_' to maintain backwards compatibility

--- circuitpython_build_tools/scripts/build_bundles.py
+++ circuitpython_build_tools/scripts/build_bundles.py
@@ -136,7 +136,7 @@ def _find_libraries(current_path, depth):
 @click.option('--output_directory', default="bundles", help="Output location for the zip files.")
 @click.option('--library_location', required=True, help="Location of libraries to bundle.")
 @click.option('--library_depth', default=0, help="Depth of library folders. This is useful when multiple libraries are bundled together but are initially in separate subfolders.")
-@click.option('--package_folder_prefix', default=None, required=False, help="Prefix string used to determine package folders to bundle.")
+@click.option('--package_folder_prefix', default="adafruit_", help="Prefix string used to determine package folders to bundle.")
 def build_bundles(filename_prefix, output_directory, library_location, library_depth, package_folder_prefix):
     os.makedirs(output_directory, exist_ok=True)
 
