c9a7d6323e2a2a282c68283e9ff0123d1c11d164
James Carr <lesamouraipourpre@gmail.com>
2021-05-30 00:27:06 +0100

    Update Python requirement to 3.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='circuitpython-build-tools',
                 'circuitpython_build_tools.scripts'],
       package_data={'circuitpython_build_tools': ['data/mpy-cross-*']},
       zip_safe=False,
-      python_requires='>=3.4',
+      python_requires='>=3.7',
       install_requires=['Click', 'requests', 'semver'],
       entry_points='''
         [console_scripts]
