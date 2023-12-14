951b47c1327a68912bd12f6e06900eacdf0bd3fc
Jeff Epler <jepler@gmail.com>
2023-12-14 10:58:14 -0600

    require tomli at install time

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='circuitpython-build-tools',
       package_data={'circuitpython_build_tools': ['data/mpy-cross-*']},
       zip_safe=False,
       python_requires='>=3.7',
-      install_requires=['Click', 'requests', 'semver'],
+      install_requires=['Click', 'requests', 'semver', 'tomli; python_version < "3.11"'],
       entry_points='''
         [console_scripts]
         circuitpython-build-bundles=circuitpython_build_tools.scripts.build_bundles:build_bundles
